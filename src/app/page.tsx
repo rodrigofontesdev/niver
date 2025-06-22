b67347df80873d0bceac6b945d86ab18f81546bf
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-06-22 19:27:10 -0300

    feat: add navigation to the button in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-import { Clock } from 'lucide-react'
+'use client'
+
+import { Clock, CornerDownLeft } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-8">
       <div className="text-center max-w-2xl">
@@ -10,9 +15,18 @@ export default function Home() {
 
         <p className="text-xl text-black/60 mb-9">Sábado, 26 de julho de 2025 às 19h30</p>
 
-        <button className="bg-gray-800 text-white px-6 py-3 rounded text-2xl font-bold cursor-pointer hover:bg-gray-900 transition-colors mb-3">
-          Vamos lá!
-        </button>
+        <div className="relative mb-3">
+          <button
+            autoFocus
+            onClick={() => router.push('/questions')}
+            className="bg-gray-800 text-white px-6 py-3 rounded text-2xl font-bold cursor-pointer outline-none hover:bg-gray-900 transition-colors"
+          >
+            Vamos lá!
+          </button>
+          <span className="absolute top-1/2 right-18 -translate-x-1/2 -translate-y-1/2 flex items-center gap-1 text-sm text-black">
+            Aperte Enter <CornerDownLeft size={14} />
+          </span>
+        </div>
 
         <p className="text-sm text-black flex items-center tracking-wider justify-center gap-1">
           <Clock size={16} />
