521c2902a7cecc0b846854c3ac4579682358f642
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-06-23 19:32:16 -0300

    refactor: remove unecessary use client directive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-'use client'
-
 import { Clock, CornerDownLeft } from 'lucide-react'
-import { useRouter } from 'next/navigation'
 
 export default function Home() {
-  const router = useRouter()
-
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4">
       <div className="max-w-2xl text-center">
@@ -18,14 +13,15 @@ export default function Home() {
         </p>
 
         <div className="w-[calc(100%-2rem)] absolute bottom-4 left-1/2 -translate-x-1/2 sm:w-auto sm:relative sm:bottom-auto sm:left-auto sm:translate-x-0">
-          <div className="relative mb-3">
-            <button
-              autoFocus
-              onClick={() => router.push('/questions')}
+          <div className="relative flex justify-center mb-3">
+            <a
+              role="button"
+              href="/questions"
               className="w-full sm:w-auto bg-gray-800 hover:bg-gray-900 text-white text-2xl font-bold px-6 py-3 rounded cursor-pointer outline-none transition-colors"
+              autoFocus
             >
               Vamos lá!
-            </button>
+            </a>
             <span className="absolute top-1/2 right-18 -translate-1/2 hidden sm:flex items-center gap-1 text-sm">
               Aperte Enter <CornerDownLeft size={14} />
             </span>
