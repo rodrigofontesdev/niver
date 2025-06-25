56cdb24948c87b006294064dd8809fa72bf100d5
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-06-24 21:02:24 -0300

    feat: add service to create an answer

---
+++ src/services/create-answer.ts
@@ -0,0 +1,52 @@
+'use server'
+
+import { notion } from '@/libs/notion-client'
+import { redirect } from 'next/navigation'
+
+export interface Answer {
+  name: string
+  attendance: string
+}
+
+export enum AttendanceOptions {
+  A = 'Com certeza!',
+  B = 'Não posso',
+}
+
+const { NOTION_DATABASE_ID } = process.env
+
+export async function createAnswer(formData: Answer) {
+  try {
+    const { name, attendance } = formData
+
+    if (!name || !attendance) {
+      throw new Error('Os campos nome ou resposta não foram informados.')
+    }
+
+    await notion.pages.create({
+      parent: {
+        database_id: NOTION_DATABASE_ID ?? '',
+      },
+      properties: {
+        Nome: {
+          title: [
+            {
+              text: {
+                content: name,
+              },
+            },
+          ],
+        },
+        Resposta: {
+          select: {
+            name: attendance === 'A' ? AttendanceOptions.A : AttendanceOptions.B,
+          },
+        },
+      },
+    })
+  } catch (error) {
+    console.error('Erro ao salvar no Notion:', error)
+  } finally {
+    redirect('/thank-you')
+  }
+}
